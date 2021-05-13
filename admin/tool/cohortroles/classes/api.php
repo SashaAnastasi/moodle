3b070471fbe0db4ecd126d700c319fff60c18ecc
Sasha Anastasi <83896190+SashaAnastasi@users.noreply.github.com>
2021-05-13 13:46:56 +1200

    MDL-60160 TOOL_COHORTROLES: fix cohort_role_sync selecting deleted users
    Prior behaviour: the cohort role sync scheduled task would attempt to
    include deleted users.
    This patch adds a check to the SQL select, omitting deleted users from
    the cohort role sync.

--- admin/tool/cohortroles/classes/api.php
+++ admin/tool/cohortroles/classes/api.php
@@ -162,6 +162,7 @@ class api {
             array_push($info[$cra->get('userid')][$cra->get('roleid')], $cra->get('cohortid'));
         }
         // Then for each user+role combo - find user context in the cohort without a role assigned.
+        //MDL-60160: Added filter for deleted user.
 
         foreach ($info as $userid => $roles) {
             foreach ($roles as $roleid => $cohorts) {
@@ -179,6 +180,7 @@ class api {
                            AND ra.roleid = :roleid
                            AND ra.userid = :userid
                          WHERE cm.cohortid ' . $cohortsql . '
+                           AND u.deleted = 1
                            AND ra.id IS NULL';
 
                 $toadd = $DB->get_records_sql($sql, $params);
